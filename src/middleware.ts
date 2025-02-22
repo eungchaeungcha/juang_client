35fb812e163430cbaef11db44dcd1c0b2a7ae507
Sejin Cha <chasj0326@naver.com>
2025-02-22 15:29:59 +0900

    refactor: 주석 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,18 +7,6 @@ import {
 
 const TOKEN_KEY = process.env.AUTH_TOKEN_KEY ?? "";
 
-export const config = {
-  matcher: [
-    /*
-     * 다음으로 시작하는 경로를 제외한 모든 요청 경로를 매칭합니다:
-     * - _next/static (정적 파일)
-     * - _next/image (이미지 최적화 파일)
-     * - favicon.ico (파비콘 파일)
-     */
-    "/((?!_next/static|_next/image|favicon.ico).*)",
-  ],
-};
-
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
   const { pathname } = request.nextUrl;
@@ -26,6 +14,7 @@ export function middleware(request: NextRequest) {
   const authToken = request.cookies.get(TOKEN_KEY)?.value;
 
   if (pathname.startsWith("/api")) {
+    // api 요청의 경우 헤더에 토큰 추가
     const requestHeaders = new Headers(request.headers);
     if (authToken) {
       requestHeaders.set("Authorization", `Bearer ${authToken}`);
@@ -59,3 +48,15 @@ export function middleware(request: NextRequest) {
 
   return response;
 }
+
+export const config = {
+  matcher: [
+    /*
+     * 다음으로 시작하는 경로를 제외한 모든 요청 경로를 매칭합니다:
+     * - _next/static (정적 파일)
+     * - _next/image (이미지 최적화 파일)
+     * - favicon.ico (파비콘 파일)
+     */
+    "/((?!_next/static|_next/image|favicon.ico).*)",
+  ],
+};
