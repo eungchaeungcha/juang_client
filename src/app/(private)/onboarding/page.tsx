e84ce62fd0e8ca3caf89acabfb6dbd7e66b1d9cb
Sejin Cha <chasj0326@naver.com>
2025-03-04 01:23:06 +0900

    rename: redirect 함수 페이지 내부 코드로 이동

--- src/app/(private)/onboarding/page.tsx
+++ src/app/(private)/onboarding/page.tsx
@@ -1,5 +1,13 @@
-import { redirectByUserData } from "@/services/redirect";
+import { redirect } from "next/navigation";
+import { usersApi } from "@/services";
+import { routePaths } from "@/constants/route";
 
 export default async function Page() {
-  await redirectByUserData();
+  const { characterId, nickName, familyId } = await usersApi.getUser();
+
+  if (!characterId) redirect(routePaths.private.onboardingCharacter);
+  if (!nickName) redirect(routePaths.private.onboardingNickname);
+  if (!familyId) redirect(routePaths.private.onboardingFamily);
+
+  redirect(routePaths.withQueryParams.treeWithId(familyId));
 }
--- src/app/(start)/page.tsx
+++ src/app/(start)/page.tsx
@@ -1,5 +1,10 @@
-import { redirectByUserData } from "@/services/redirect";
+import { redirect } from "next/navigation";
+import { usersApi } from "@/services";
+import { routePaths } from "@/constants/route";
 
 export default async function Page() {
-  await redirectByUserData();
+  const { familyId } = await usersApi.getUser();
+
+  if (!familyId) redirect(routePaths.private.onboarding);
+  redirect(routePaths.withQueryParams.treeWithId(familyId));
 }
